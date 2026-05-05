98f689c7b2409c5254dbe1dc6f84318a3fe07068
Neila <neila.asynur@gmail.com>
2026-05-05 15:35:41 +0700

    update soal 4

--- Latihan/src/AcPintar.java
+++ Latihan/src/AcPintar.java
@@ -3,4 +3,8 @@ public class AcPintar  extends PerangkatPintar{
     void aktifkan () {
         System.out.println("AC menyala dan mulai mendinginkan ruangan.");
     }
+
+    void aturSuhu (int suhu){
+        System.out.println("Suhu ruangan diatur menjadi " + suhu + " derajat.");
+    }
 }
--- Latihan/src/Main.java
+++ Latihan/src/Main.java
@@ -6,9 +6,13 @@ public class Main {
         objekList.add(new LampuPintar());
 
         for (PerangkatPintar list : objekList){
+            if (list instanceof AcPintar){
+                AcPintar objekAc = (AcPintar) list;
+                objekAc.aturSuhu(20);
+            }
             list.aktifkan();
         }
-        // hasil output yang berbeda dikarenakan adanya konsep overriding dan overloading
+        // hasil output yang berbeda dikarenakan adanya konsep overriding
         // hal ini terjadi karena method anak/child menimpa method dari parentnya.
         // sehingga, meskipun method yang dipanggil berdasarkann referensi dari class parent, tetapi method yang dijalankan tetap berdasarkan objek aslinya
     }
