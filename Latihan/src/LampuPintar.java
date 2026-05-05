b57b64e1897555927d4ac24bc4caa6cb3a4fecdd
Neila <neila.asynur@gmail.com>
2026-05-05 15:10:14 +0700

    update soal 2

--- Latihan/src/LampuPintar.java
+++ Latihan/src/LampuPintar.java
@@ -3,4 +3,11 @@ public class LampuPintar extends PerangkatPintar{
     void aktifkan (){
         System.out.println("Lampu menyala dengan tingkat kecerahan standar.");
     }
+
+    void aturKecerahan (int level){
+        System.out.println("Kecerahan lampu diatur ke level " + level + "%.");
+    }
+    void aturKecerahan (int level, String warna){
+        System.out.println("Kecerahan lampu diatur ke level" + level + "% dengan warna cahaya " + warna + ".");
+    }
 }
